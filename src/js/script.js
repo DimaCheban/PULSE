79ab029a2fa93ef701c5379fec4b4917afccc2c4
Dima <dmitrymiless@gmail.com>
2023-03-31 15:40:57 +0300

    I have started to connect the slider to the website and have linked all the necessary links. At the moment, I am in the early stages of configuring the slider.

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+       speed: 1200,
+       adaptiveHeight: true, 
+       prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></buttton>',
+       nextArrow: '<button type="button" class="slick-prev"><img src="../icons/right.svg"></buttton>'
+      });
+  });
