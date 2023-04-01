4a7cdefff373a260dd529681a5972c90751d7147
Dima <dmitrymiless@gmail.com>
2023-04-01 12:09:44 +0300

    uperficially adapted the slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
        speed: 1200,
        adaptiveHeight: true, 
        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></buttton>',
-       nextArrow: '<button type="button" class="slick-prev"><img src="../icons/right.svg"></buttton>'
+       nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></buttton>',
+       responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    // $lidesToShow: 3,
+                    // slidesToScroll: 3,
+                    // infinity: true,
+                    dots: true,
+                    arrows: false
+                }
+            }
+       ]
       });
   });
